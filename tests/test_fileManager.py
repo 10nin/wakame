4277919a522232122a4cd7a3c9d042d1a8d889bd
Akira Ota 10nin <akira19870221@gmail.com>
2017-11-09 23:56:10 +0900

    Add test for get_hash method at none exists file.

--- tests/test_fileManager.py
+++ tests/test_fileManager.py
@@ -7,3 +7,8 @@ class TestFileManager(TestCase):
     def test_get_hash_for_test1(self):
         fm = FileManager()
         self.assertEqual('F7A17082FFBBFBBE6A6B4474EF7C6045B3F6D009', fm.get_hash('test_files/test1.rst'))
+
+    def test_get_hash_for_none_exists_file(self):
+        fm = FileManager()
+        with self.assertRaises(OSError):
+            fm.get_hash('')
